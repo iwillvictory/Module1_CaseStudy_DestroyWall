d05e9c239841ebfcfaa848ff68d1d0fb2e39a8ce
QUYNHNB <nbquynh911@gmail.com>
2019-09-02 06:52:41 +0700

    update

--- actions/initGame.js
+++ actions/initGame.js
@@ -11,11 +11,11 @@ let ctx = canvas.getContext("2d");
 let levelElement=document.getElementById("selectLevel");
 
 let level1=new Level(0.022,0.022,0.022, 0.1,0.05,4,8,
-    12,5,5,200,10);
+    12,5,6,180,10);
 let level2=new Level(0.012,0.012,0.012, 0.07,0.03,6,12,
-    10,7,7,150,15);
+    12,7,8,150,15);
 let level3=new Level(0.011,0.011,0.012, 0.05,0.02,8,16,
-    8,10,10,150,20);
+    12,9,10,130,20);
 
 
 let isGameWin=false;
